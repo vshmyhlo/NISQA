5273faec2c2b8c069b8b28b7770235ef152f1daf
vlad <vlad@cohere.com>
2026-04-16 11:06:06 +0000

    update init

--- nisqa/__init__.py
+++ nisqa/__init__.py
@@ -1,13 +1,4 @@
+from .NISQA_model import nisqaModel
+from .inference import NISQAPredictor
+
 __all__ = ["nisqaModel", "NISQAPredictor"]
-
-
-def __getattr__(name):
-    if name == "nisqaModel":
-        from .NISQA_model import nisqaModel
-
-        return nisqaModel
-    if name == "NISQAPredictor":
-        from .inference import NISQAPredictor
-
-        return NISQAPredictor
-    raise AttributeError("module 'nisqa' has no attribute '{}'".format(name))
